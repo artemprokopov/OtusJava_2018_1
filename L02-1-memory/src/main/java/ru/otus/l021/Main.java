3b7596a7a4fb6964300a76b322dca345d1fdfabf
Artem Prokopov <art1828@mail.ru>
2018-02-14 18:31:53 +0300

    Работа над домашним заданием урок 2

--- L02-1-memory/src/main/java/ru/otus/l021/Main.java
+++ L02-1-memory/src/main/java/ru/otus/l021/Main.java
@@ -3,109 +3,84 @@ package ru.otus.l021;
 import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 
 import java.lang.management.ManagementFactory;
-import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 
 public class Main {
+    private static char[] EMTY_CHAR_ARRAY = new char[0];
     public static void main(String[] args) throws InterruptedException,
             IllegalAccessException,
             InstantiationException,
             NoSuchMethodException,
             InvocationTargetException {
+        printPid();
         Main main = new Main();
-        main.printPid();
-        main.watchSizeMemoryObject(new Object());
-        main.watchSizeMemoryObjectWithRuntime(Object.class, 0);
-        main.watchSizeMemoryObject(new String(""));
-        main.watchSizeMemoryObjectWithRuntime(String.class, 0);
-        main.watchSizeMemoryObject(new int[1]);
-        main.watchSizeMemoryObjectWithRuntime(int[].class, 0);
+        long sizeObjectRuntime = main.watchSizeMemoryObjectWithRuntime(new Object());
+        long sizeObjectSizeCalculator = main.watchSizeMemoryObject(new Object());
+        System.out.println("Size object of " + Object.class
+                + " equals to " + sizeObjectRuntime + " byte" + " with use Runtime");
+        System.out.println("Size object of " + Object.class
+                + " equals to " + sizeObjectSizeCalculator + " byte" + " with use ObjectSizeCalculator");
+        sizeObjectRuntime = main.watchSizeMemoryObjectWithRuntime(new String());
+        sizeObjectSizeCalculator = main.watchSizeMemoryObject(new String());
+        System.out.println("Size object of " + String.class
+                + " equals to " + sizeObjectRuntime + " byte" + " with use Runtime");
+        System.out.println("Size object of " + String.class
+                + " equals to " + sizeObjectSizeCalculator + " byte" + " with use Runtime");
     }
 
-    private void watchSizeMemoryObject(Object object)  {
+    public long watchSizeMemoryObject(Object object)  {
         if (Objects.isNull(object)) {
-            return;
+            return -1;
         }
-        System.out.println("Size object of "
-                + object.getClass() + " equals to "
-                + ObjectSizeCalculator.getObjectSize(object) + " byte"
-                + " with use ObjectSizeCalculator");
+        return ObjectSizeCalculator.getObjectSize(object);
     }
 
-    private void  watchSizeMemoryObjectWithRuntime(Class cl, int arraySize) throws InterruptedException,
+    public long  watchSizeMemoryObjectWithRuntime(Object object) throws InterruptedException,
             IllegalAccessException,
             InstantiationException,
             NoSuchMethodException,
             InvocationTargetException {
-
-        long sizeObject = 0;
-        if (cl == String.class) {
-            sizeObject = getSizeStringObject(cl);
-        } else if (cl == int[].class) {
-            sizeObject = getSizeArrayObject(cl, arraySize);
-        } else {
-            sizeObject = getSizeSimpleObject(cl);
+        if (Objects.isNull(object)) {
+            return -1;
         }
-        System.out.println("Size object of "
-                + cl + " equals to "
-                + sizeObject + " byte"
-                + " with use Runtime");
-
+        int size = 2_000_000;
+        Object[] array = new Object[size];
+        runGC();
+        long startSizeMemory = getSizeMemory();
+        for (int i = 0; i < size; i++) {
+            array[i] = GetObject.getNeededTypeOfObject(object);
+        }
+        long finishSizeMemory = getSizeMemory();
+        long differencesizeMemory = finishSizeMemory- startSizeMemory;
+        return differencesizeMemory / size;
     }
 
-    private void printPid() {
+    private static void printPid() {
         System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
     }
 
-    private long getSizeSimpleObject(Class cl) throws InterruptedException,
-            IllegalAccessException,
-            InstantiationException {
-        int size = 2_000_000;
-        Thread.sleep(100);
-        Runtime runtime = Runtime.getRuntime();
-        Object[] array = new Object[size];
-        long startMemorySize = runtime.maxMemory() - runtime.freeMemory();
-        for (int i = 0; i < size; i++) {
-            array[i] = cl.newInstance();
+    private static class GetObject {
+
+        public static <T> Object getNeededTypeOfObject(T neededTypeOfObject) {
+            if (neededTypeOfObject.getClass() == String.class) {
+                return new String(EMTY_CHAR_ARRAY);
+            }
+            if (neededTypeOfObject instanceof Object[]) {
+                return ((Object[]) neededTypeOfObject).clone();
+            }
+            return new Object();
         }
-        long finishMemorySize = runtime.maxMemory() - runtime.freeMemory();
-        return  (finishMemorySize - startMemorySize) / size;
     }
 
-    private long getSizeArrayObject(Class cl, int size) throws InterruptedException {
+    private static void runGC() throws InterruptedException {
+        System.gc();
+        System.runFinalization();
+        System.gc();
         Thread.sleep(100);
-        Runtime runtime = Runtime.getRuntime();
-        long startMemorySize = runtime.maxMemory() - runtime.freeMemory();
-        Object newArray = Array.newInstance(cl, size);
-        long finishMemorySize = runtime.maxMemory() - runtime.freeMemory();
-        return  finishMemorySize - startMemorySize;
     }
 
-    private long getSizeStringObject(Class cl) throws InterruptedException,
-            IllegalAccessException,
-            InstantiationException,
-            NoSuchMethodException,
-            InvocationTargetException {
-        int size = 8;
-        Thread.sleep(100);
-        Runtime runtime = Runtime.getRuntime();
-        System.gc();
-        System.gc();
-        System.gc();
-        System.gc();
-        long startMemorySize = runtime.maxMemory() - runtime.freeMemory();
-        String st1 = new String(new char[0]);
-        String st2 = new String(new char[0]);
-        String st3 = new String(new char[0]);
-        String st4 = new String(new char[0]);
-        String st5 = new String(new char[0]);
-        String st6 = new String(new char[0]);
-        String st7 = new String(new char[0]);
-        String st8 = new String(new char[0]);
-        long finishMemorySize = runtime.maxMemory() - runtime.freeMemory();
-        long diferentMemorySize = finishMemorySize - startMemorySize;
-        long result = Math.round(((double) diferentMemorySize )/ size);
-        return  result;
+    private static long getSizeMemory() {
+        return Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
     }
 }
