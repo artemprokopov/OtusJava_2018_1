8c5039fc3caefa6bdd2bd32a476aae843e47f898
Artem Prokopov <art1828@mail.ru>
2018-03-13 23:31:28 +0300

    Задание l041 тест gc.

---
+++ L04-1-gc/src/main/java/ru/otus/l041/script/L04-1-gcParallelGC.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+TYPE_GC="-XX:+UseParallelGC"
+TYPE_PROG="-jar"
+MAIN_FILE="L04-1-gc.jar"
+MIN_MEM="-Xms256m"
+MAX_MEM="-Xmx256m"
+
+java ${TYPE_PROG} ${MIN_MEM} ${MAX_MEM} ${TYPE_GC} ${MAIN_FILE}
---
+++ L04-1-gc/src/main/java/ru/otus/l041/script/L04-1-gcSerialGC.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set TYPE_GC=-XX:+UseSerialGC
+set TYPE_PROG=-jar
+set MAIN_FILE=L04-1-gc.jar
+set MIN_MEM=-Xms256m
+set MAX_MEM=-Xmx256m
+
+java %TYPE_PROG% %MIN_MEM% %MAX_MEM% %TYPE_GC% %MAIN_FILE%
