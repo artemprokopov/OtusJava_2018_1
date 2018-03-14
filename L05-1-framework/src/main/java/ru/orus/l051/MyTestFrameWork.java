e93df59c79a19f21b70fa50f07ac94a7e5f62a7f
Artem Prokopov <art1828@mail.ru>
2018-03-14 22:23:07 +0300

    Задание l051 testframework.

---
+++ L05-1-framework/src/main/java/ru/orus/l051/MyTestFrameWork.java
@@ -0,0 +1,11 @@
+package ru.orus.l051;
+
+
+
+public class MyTestFrameWork {
+    public static void main(String[] args) {
+        Package testablePackage = Package.getPackage(args[0]);
+//        MyTestClass[] testableClass = testablePackage.getAnnotationsByType(MyTestClass.class);
+//        Class.forName(testableClass[0].getClass().getName()).
+    }
+}
