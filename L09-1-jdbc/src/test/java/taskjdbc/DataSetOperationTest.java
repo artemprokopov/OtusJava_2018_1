adb5666580e63c2784ac0dca976daa256d6e28a5
Artem Prokopov <art1828@mail.ru>
2018-04-30 19:13:58 +0300

    L09 JDBC.

--- L09-1-jdbc/src/test/java/taskjdbc/DataSetOperationTest.java
+++ L09-1-jdbc/src/test/java/taskjdbc/DataSetOperationTest.java
@@ -15,7 +15,8 @@ public class DataSetOperationTest {
      */
     private final String mySqlUrl = "jdbc:mysql://localhost:3306/otus"
             + "?useUnicode=true&useJDBCCompliantTimezoneShift=true"
-            + "&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=true";
+            + "&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=true"
+            + "&autoReconnect=true&failOverReadOnly=false&maxReconnects=10";
     /**
      * Поле имени пользователя к тестовой базе.
      */
