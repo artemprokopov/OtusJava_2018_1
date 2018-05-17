95011644ead6020448458dc14180746a250b8e99
Artem Prokopov <art1828@mail.ru>
2018-05-18 01:09:48 +0300

    Правка

--- L10-1-hibernate/src/main/java/ru/otus/hebertask/AddressDataSet.java
+++ L10-1-hibernate/src/main/java/ru/otus/hebertask/AddressDataSet.java
@@ -4,8 +4,6 @@ package ru.otus.hebertask;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.Objects;
 
